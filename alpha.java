f89261652814fd7361e0230c39139a1cd724fc99
jharna12 <40167280+jharna12@users.noreply.github.com>
2019-01-05 21:13:35 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class number
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+char n=sc.next().charAt(0);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+System.out.println("vowel");
+else 
+System.out.println("consonants");
+
+}
+}
