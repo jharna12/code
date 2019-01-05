2271c012f7bc0b87f388d01993a8e8bf2c2a667b
jharna12 <40167280+jharna12@users.noreply.github.com>
2019-01-05 17:53:06 +0530

    Add files via upload

---
+++ countk1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class number
+{
+    public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int l=String.valueOf(n).length();
+System.out.println(l);
+}
+}
